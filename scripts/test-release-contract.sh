ae50efeb63b3df641be84291cd30e9a9b928b02f
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-07-24 02:45:22 -0300

    ci(release): pin corrected release action

--- scripts/test-release-contract.sh
+++ scripts/test-release-contract.sh
@@ -10,7 +10,7 @@ test "$(jq -r '.name' "$metadata_path")" = "delivery_module"
 grep -Fq "std::string moduleVersion = \"${version}\";" src/delivery_module_plugin.cpp
 grep -Fq "## [${version}]" CHANGELOG.md
 grep -Fq 'test "$GITHUB_REF" = "refs/heads/master"' "$release_workflow"
-grep -Fq 'uses: 3esmit/logos-modules-release-action/.github/workflows/release.yml@670e3d8b704207da88947d3417767fa34a9c5e28' "$release_workflow"
+grep -Fq 'uses: 3esmit/logos-modules-release-action/.github/workflows/release.yml@7e09ddf90bf33ce29ef3505609332175a6cd06c9' "$release_workflow"
 grep -Fq 'variants: linux-amd64,darwin-arm64' "$release_workflow"
 grep -Fq 'require_all_variants: true' "$release_workflow"
 grep -Fq 'dispatch_rebuild_index: false' "$release_workflow"
