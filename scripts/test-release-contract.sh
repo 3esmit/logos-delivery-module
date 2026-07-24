52fed008b035265ee407806cadea936597d3ed41
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-07-24 04:25:06 -0300

    ci(release): pin sidecar helper fix

--- scripts/test-release-contract.sh
+++ scripts/test-release-contract.sh
@@ -10,7 +10,7 @@ test "$(jq -r '.name' "$metadata_path")" = "delivery_module"
 grep -Fq "std::string moduleVersion = \"${version}\";" src/delivery_module_plugin.cpp
 grep -Fq "## [${version}]" CHANGELOG.md
 grep -Fq 'test "$GITHUB_REF" = "refs/heads/master"' "$release_workflow"
-grep -Fq 'uses: 3esmit/logos-modules-release-action/.github/workflows/release.yml@7e09ddf90bf33ce29ef3505609332175a6cd06c9' "$release_workflow"
+grep -Fq 'uses: 3esmit/logos-modules-release-action/.github/workflows/release.yml@81f506530c56e8757e6d99ee7f9d4c092e74411c' "$release_workflow"
 grep -Fq 'variants: linux-amd64,darwin-arm64' "$release_workflow"
 grep -Fq 'require_all_variants: true' "$release_workflow"
 grep -Fq 'dispatch_rebuild_index: false' "$release_workflow"
